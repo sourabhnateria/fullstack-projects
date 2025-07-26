041ff76a5df923e93d076d99fd9a3789bd20d1d9
sourabh nateria <sourabhnateria.cse@gmail.com>
2025-07-26 10:01:18 +0530

    Update Projects.js

--- portfolio-project/client/src/components/Projects.js
+++ portfolio-project/client/src/components/Projects.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import axios from 'axios';
-import Modal from 'react-modal';
 import './Projects.css';
 
 const Projects = () => {
