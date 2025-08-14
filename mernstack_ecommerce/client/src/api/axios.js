19b84ac94c32625476afc344f2153eed20ba5c8e
sourabh nateria <sourabhnateria.cse@gmail.com>
2025-08-14 18:07:08 +0530

    Update axios.js

--- mernstack_ecommerce/client/src/api/axios.js
+++ mernstack_ecommerce/client/src/api/axios.js
@@ -1,1 +1,8 @@
+// src/api/axiosClient.js
+import axios from 'axios';
 
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+});
+
+export default axiosClient;
